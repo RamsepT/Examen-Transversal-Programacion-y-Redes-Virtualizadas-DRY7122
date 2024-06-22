6e56a3515ba593a4cb504d5ac341f121d5e24415
RamsepT <ramseptvegap@gmail.com>
2024-06-22 16:32:53 -0400

    Add files via upload

---
+++ integrantesET.py
@@ -0,0 +1,13 @@
+# integrantesET.py
+
+def imprimir_integrantes():
+    integrantes = [
+        "Jeremy Soto",
+        "Rodrigo Flores",
+        "Ramsept Vega"
+    ]
+    for integrante in integrantes:
+        print(integrante)
+
+if __name__ == "__main__":
+    imprimir_integrantes()
